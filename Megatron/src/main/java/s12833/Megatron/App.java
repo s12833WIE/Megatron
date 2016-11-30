c6c13729b10526fef2c4a20ea0381f6e0b644ec3
s12833WIE <michuwiecz@gmail.com>
2016-11-30 14:08:21 +0100

    first commit

---
+++ Megatron/src/main/java/s12833/Megatron/App.java
@@ -0,0 +1,13 @@
+package s12833.Megatron;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
